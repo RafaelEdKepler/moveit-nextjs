c4b4e57c99c09e21873d0dc570c2c81b3b70e9bc
Rafael Eduardo Kepler <rafael.kepler@hotmail.com>
2021-02-25 07:55:15 -0300

    Segunda aula NLW React

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return (
+        <div>
+            <img src="https://github.com/rafaeledkepler" />
+            <div>
+                <strong>Rafael Eduardo Kepler</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
-import Head from 'next/head'
-import { ExperienceBar } from '../components/ExperienceBar'
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" />
-      </Head>
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
